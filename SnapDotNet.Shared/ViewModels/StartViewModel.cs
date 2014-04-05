361db7fa64acc23e0b8eaa4530bc9a764ee66b2e
Matt <msaville8@gmail.com>
2014-04-05 19:33:47 -0400

    Added registration form

--- SnapDotNet.Shared/ViewModels/StartViewModel.cs
+++ SnapDotNet.Shared/ViewModels/StartViewModel.cs
@@ -9,37 +9,86 @@ namespace SnapDotNet.Apps.ViewModels
     {
 		public StartViewModel()
 		{
-			OpenSignInPageCommand = new RelayCommand(() =>
-			{
-				IsSignInPageVisible = true;
-			});
+			IsStartPageVisible = true;
+
+			OpenSignInPageCommand = new RelayCommand(
+				() =>
+				{
+					IsSignInPageVisible = true;
+					IsStartPageVisible = false;
+				},
+				() => { return IsStartPageVisible; }
+			);
+
+			OpenRegisterPageCommand = new RelayCommand(
+				() =>
+				{
+					IsRegisterPageVisible = true;
+					IsStartPageVisible = false;
+				},
+				() => { return IsStartPageVisible; }
+			);
 
 			GoBackToStartCommand = new RelayCommand(() =>
 			{
 				IsSignInPageVisible = false;
 				IsRegisterPageVisible = false;
+				IsStartPageVisible = true;
 			});
+
+			SignInCommand = new RelayCommand(SignIn);
+			RegisterCommand = new RelayCommand(Register);
 		}
 
 		/// <summary>
-		/// Gets or sets the command to open the sign in page.
+		/// Gets the command to open the sign in form.
 		/// </summary>
 		public ICommand OpenSignInPageCommand
 		{
 			get { return _openSignInPageCommand; }
-			set { SetField(ref _openSignInPageCommand, value); }
+			private set { SetField(ref _openSignInPageCommand, value); }
 		}
 		private ICommand _openSignInPageCommand;
 
 		/// <summary>
-		/// Gets or sets the command to go back to the start page.
+		/// Gets the command to open the registration form.
+		/// </summary>
+		public ICommand OpenRegisterPageCommand
+		{
+			get { return _openRegisterPageCommand; }
+			private set { SetField(ref _openRegisterPageCommand, value); }
+		}
+		private ICommand _openRegisterPageCommand;
+
+		/// <summary>
+		/// Gets the command to go back to the start page.
 		/// </summary>
 		public ICommand GoBackToStartCommand
 		{
 			get { return _goBackToStartCommand; }
-			set { SetField(ref _goBackToStartCommand, value); }
+			private set { SetField(ref _goBackToStartCommand, value); }
 		}
 		private ICommand _goBackToStartCommand;
+
+		/// <summary>
+		/// Gets the command to sign in.
+		/// </summary>
+		public ICommand SignInCommand
+		{
+			get { return _signInCommand; }
+			private set { SetField(ref _signInCommand, value); }
+		}
+		private ICommand _signInCommand;
+
+		/// <summary>
+		/// Gets the command to register.
+		/// </summary>
+		public ICommand RegisterCommand
+		{
+			get { return _registerCommand; }
+			private set { SetField(ref _registerCommand, value); }
+		}
+		private ICommand _registerCommand;
 
 		/// <summary>
 		/// Gets whether the sign in form should be visible.
@@ -60,5 +109,31 @@ namespace SnapDotNet.Apps.ViewModels
 			private set { SetField(ref _isRegisterPageVisible, value); }
 		}
 		private bool _isRegisterPageVisible;
+
+		/// <summary>
+		/// Gets whether the starting menu should be visible.
+		/// </summary>
+		public bool IsStartPageVisible
+		{
+			get { return _isStartPageVisible; }
+			private set { SetField(ref _isStartPageVisible, value); }
+		}
+		private bool _isStartPageVisible;
+
+		/// <summary>
+		/// Attempts to sign the user into Snapchat.
+		/// </summary>
+		private void SignIn()
+		{
+
+		}
+
+		/// <summary>
+		/// Attempts to create a new account.
+		/// </summary>
+		private void Register()
+		{
+
+		}
     }
 }
