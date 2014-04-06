26350ead9fdfdcca8ef64d3ed61b48412fe111ad
Matt <msaville8@gmail.com>
2014-04-05 20:21:14 -0400

    Started work on main page and added stuff to the settings flyout

--- SnapDotNet.Windows/Pages/StartPage.xaml.cs
+++ SnapDotNet.Windows/Pages/StartPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -36,6 +37,26 @@ namespace SnapDotNet.Apps.Pages
 				else
 					VisualStateManager.GoToState(this, "DefaultLayout", true);
 			};
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			SettingsPane.GetForCurrentView().CommandsRequested += OnSettingsCommandsRequested;
+			base.OnNavigatedTo(e);
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			SettingsPane.GetForCurrentView().CommandsRequested -= OnSettingsCommandsRequested;
+			base.OnNavigatedFrom(e);
+		}
+
+		void OnSettingsCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+		{
+			args.Request.ApplicationCommands.Add(new SettingsCommand("privacy_policy", "Privacy policy", delegate
+			{
+				// TODO: Open privacy policy
+			}));
 		}
 	}
 }
