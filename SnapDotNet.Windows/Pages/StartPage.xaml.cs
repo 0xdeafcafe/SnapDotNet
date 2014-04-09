5b1dac7bb86bc4e4f2acaeefb7012dabc7b30510
Alex Reed (Xerax) <alexreed@outlook.com>
2014-04-09 20:45:02 +0100

    Background stuff for Windows

--- SnapDotNet.Windows/Pages/StartPage.xaml.cs
+++ SnapDotNet.Windows/Pages/StartPage.xaml.cs
@@ -1,19 +1,7 @@
-﻿using SnapDotNet.Apps.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.UI.Xaml.Media.Animation;
+using SnapDotNet.Apps.ViewModels;
 using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -23,25 +11,22 @@ namespace SnapDotNet.Apps.Pages
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class StartPage : Page
+	public sealed partial class StartPage
 	{
 		public StartPage()
 		{
 			DataContext = new StartViewModel();
 
-			this.InitializeComponent();
-			this.SizeChanged += (object sender, SizeChangedEventArgs e) =>
-			{
-				if (e.NewSize.Width <= (int) Resources["MinimalViewMaxWidth"])
-					VisualStateManager.GoToState(this, "MinimalLayout", true);
-				else
-					VisualStateManager.GoToState(this, "DefaultLayout", true);
-			};
+			InitializeComponent();
+			SizeChanged += (sender, e) => VisualStateManager.GoToState(this,
+				e.NewSize.Width <= (int) Resources["MinimalViewMaxWidth"] ? "MinimalLayout" : "DefaultLayout", true);
 		}
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
 			SettingsPane.GetForCurrentView().CommandsRequested += OnSettingsCommandsRequested;
+			BackgroundAmbienceStoryboard.RepeatBehavior = new RepeatBehavior { Type = RepeatBehaviorType.Forever };
+			BackgroundAmbienceStoryboard.Begin();
 			base.OnNavigatedTo(e);
 		}
 
@@ -51,7 +36,7 @@ namespace SnapDotNet.Apps.Pages
 			base.OnNavigatedFrom(e);
 		}
 
-		void OnSettingsCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+		static void OnSettingsCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
 		{
 			args.Request.ApplicationCommands.Add(new SettingsCommand("privacy_policy", "Privacy policy", delegate
 			{
