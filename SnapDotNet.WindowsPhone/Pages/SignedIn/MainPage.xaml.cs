c56f9a5b1b758e9945753a5d5d22ebe53d031ea7
Alex Reed (Xerax) <alexreed@outlook.com>
2014-04-09 01:37:20 +0100

    Capture page styling

--- SnapDotNet.WindowsPhone/Pages/SignedIn/MainPage.xaml.cs
+++ SnapDotNet.WindowsPhone/Pages/SignedIn/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using Windows.Devices.Enumeration;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media.Capture;
 
@@ -17,14 +19,15 @@ namespace SnapDotNet.Apps.Pages.SignedIn
 		}
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
-			CameraPreview.Source = _mediaCapture;
-			StartCameraDevice();
+			//StartCameraDevice();
 
+			StatusBar.GetForCurrentView().BackgroundColor = new Color { A = 0x00, R = 0x00, G = 0x00, B = 0x00, };
+			ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
 		}
 
 		protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
 		{
-			_mediaCapture.StopPreviewAsync();
+			//_mediaCapture.StopPreviewAsync();
 		}
 		private async void StartCameraDevice()
 		{
@@ -44,6 +47,7 @@ namespace SnapDotNet.Apps.Pages.SignedIn
 			Debug.WriteLine("Initialising Camera: OK");
 
 			Debug.WriteLine("Starting Camera Preview");
+			//CameraPreview.Source = _mediaCapture;
 			await _mediaCapture.StartPreviewAsync();
 			Debug.WriteLine("Starting Camera Preview: OK");
 		}
