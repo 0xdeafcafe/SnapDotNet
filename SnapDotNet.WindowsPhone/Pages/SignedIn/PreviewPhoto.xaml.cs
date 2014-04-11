b7239fb1bd3188c43ae961c95180a7cebcecaa5d
Kaelan Fouwels <kfouwels@outlook.com>
2014-04-11 16:27:21 +0100

    Should probably git add before committing, derp

---
+++ SnapDotNet.WindowsPhone/Pages/SignedIn/PreviewPhoto.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace SnapDotNet.Apps.Pages.SignedIn
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class PreviewPhoto : Page
+	{
+		public PreviewPhoto()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			var stream = e.Parameter as InMemoryRandomAccessStream;
+			Debug.WriteLine("Navigated to PhotoPreview, caught stream of size {0}", stream.Size);
+
+			var bmpimg = new BitmapImage();
+			bmpimg.ImageFailed += bmpimg_ImageFailed;
+			bmpimg.SetSource(stream);
+			ImageReview.Source = bmpimg;
+		}
+
+		void bmpimg_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+		{
+			Debug.WriteLine("Image Decoding Failed");
+		}
+	}
+}
---
+++ SnapDotNet.WindowsPhone/Pages/SignedIn/PreviewVideo.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace SnapDotNet.Apps.Pages.SignedIn
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class PreviewVideo : Page
+	{
+		public PreviewVideo()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			var stream = e.Parameter as InMemoryRandomAccessStream;
+			Debug.WriteLine("Navigated to VideoPreview, caught stream of size {0}", stream.Size);
+			//MediaReview.Source = stream;
+
+		}
+	}
+}
