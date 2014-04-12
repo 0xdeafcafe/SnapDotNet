00934cd0c81409849c724698d299b6f9af54ca54
Collin Dillinger <collin.dillinger@gmail.com>
2014-04-12 02:29:56 -0400

    Image test

--- SnapDotNet.Core.Snapchat.Api.Tests/EndpointTests.cs
+++ SnapDotNet.Core.Snapchat.Api.Tests/EndpointTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using SnapDotNet.Core.Snapchat.Api.Tests.Helpers;
 
 namespace SnapDotNet.Core.Snapchat.Api.Tests
 {
@@ -8,17 +7,12 @@ namespace SnapDotNet.Core.Snapchat.Api.Tests
 	public class EndpointTests
 	{
 		[TestMethod]
-		public async Task GetUpdatesAsyncTest()
+		public async Task RegisterReturnCaptchaAsyncTest()
 		{
-			var authentication = SettingsLoader.GetAuthencationInfo();
-			var snapChatManager = new SnapChatManager(authentication.Username, authentication.AuthToken);
-			//var snapChatManager = new SnapChatManager();
-			//await snapChatManager.Endpoints.AuthenticateAsync(authentication.Username, authentication.Password);
-			var account = await snapChatManager.Endpoints.GetUpdatesAsync();
-			var image = await snapChatManager.Endpoints.GetSnapBlobAsync(account.Snaps[0]);
+			var captchaZip = await new SnapChatManager().Endpoints.RegisterAndGetCaptchaAsync(19, "1994-08-18", "i.love.xerax@yopmail.com", "iloveALEX");
+			// captchaZip is a List of byte arrays which are the data of PNG files
 
-			Assert.AreEqual(true, account.Logged);
-			Assert.AreEqual(authentication.Username.ToLowerInvariant(), account.Username.ToLowerInvariant());
+			Assert.AreEqual(true, true);
 		}
 	}
 }
