c3793109f164d645309536c34f10c364aee11ba5
Alex Reed (Xerax) <alexreed@outlook.com>
2014-04-21 18:02:48 +0100

    maybe new icon stuff?

--- SnapDotNet.Core.Miscellaneous/Helpers/ApplicationSettings.cs
+++ SnapDotNet.Core.Miscellaneous/Helpers/ApplicationSettings.cs
@@ -21,13 +21,26 @@ namespace SnapDotNet.Core.Miscellaneous.Helpers
 		}
 		private SnapAutoDownloadMode _snapAutoDownloadMode = SnapAutoDownloadMode.Wifi;
 
+		public int UnreadSnapCount
+		{
+			get { return _unreadSnapCount; }
+			set { SetField(ref _unreadSnapCount, value); }
+		}
+		private int _unreadSnapCount;
+
 		#endregion
 
 		#region Helpers
 
 		private void Load()
 		{
-			SnapAutoDownloadMode = (SnapAutoDownloadMode) LoadSettingInt("SnapAutoDownloadMode", (int) SnapAutoDownloadMode.Wifi);
+			SnapAutoDownloadMode = (SnapAutoDownloadMode)LoadSettingInt("SnapAutoDownloadMode", (int)SnapAutoDownloadMode.Wifi);
+			UnreadSnapCount = LoadSettingInt("UnreadSnapCount", 0);
+		}
+
+		private void Save()
+		{
+			IsolatedStorage.WriteLocalSetting("ApplicationSettings", "UnreadSnapCount", UnreadSnapCount.ToString());
 		}
 
 		private static string LoadSettingString(string settingName, string defaultValue)
@@ -37,11 +50,6 @@ namespace SnapDotNet.Core.Miscellaneous.Helpers
 		private static int LoadSettingInt(string settingName, int defaultValue)
 		{
 			return IsolatedStorage.ReadLocalSettingInt("ApplicationSettings", settingName, defaultValue);
-		}
-
-		private void Save()
-		{
-			IsolatedStorage.WriteLocalSetting("ApplicationSettings", "SnapAutoDownloadMode", ((int)SnapAutoDownloadMode).ToString());
 		}
 
 		#endregion
