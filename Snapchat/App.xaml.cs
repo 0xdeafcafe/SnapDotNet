c67e3f7f131302c3455363e5bc6cfa5860bcb8f3
Alex Reed (Xerax) <alexreed@outlook.com>
2014-06-04 18:40:04 +0100

    think of the tabs

--- Snapchat/App.xaml.cs
+++ Snapchat/App.xaml.cs
@@ -13,11 +13,11 @@ using SnapDotNet.Core.Snapchat.Api;
 
 namespace Snapchat
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    public sealed partial class App : Application
-    {
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public sealed partial class App
+	{
 		public static readonly ResourceLoader Strings = new ResourceLoader();
 
 		public static MobileServiceClient MobileService = new MobileServiceClient(
@@ -26,115 +26,116 @@ namespace Snapchat
 		);
 		public static string DeviceId;
 
-	    public static readonly SnapchatManager SnapchatManager = new SnapchatManager();
+		public static readonly SnapchatManager SnapchatManager = new SnapchatManager();
 
-        private TransitionCollection _transitions;
+		private TransitionCollection _transitions;
 
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            InitializeComponent();
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			InitializeComponent();
 
-            Suspending += OnSuspending;
+			Suspending += OnSuspending;
 			UnhandledException += OnUnhandledException;
-        }
+		}
 
-	    /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used when the application is launched to open a specific file, to display
-        /// search results, and so forth.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override async void OnLaunched(LaunchActivatedEventArgs e)
-        {
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used when the application is launched to open a specific file, to display
+		/// search results, and so forth.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override async void OnLaunched(LaunchActivatedEventArgs e)
+		{
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                DebugSettings.EnableFrameRateCounter = true;
-            }
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				DebugSettings.EnableFrameRateCounter = true;
+			}
 #endif
 
-            Frame rootFrame = Window.Current.Content as Frame;
+			var rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-	            rootFrame = new Frame {CacheSize = 2};
+			// Do not repeat app initialization when the Window already has content,
+			// just ensure that the window is active
+			if (rootFrame == null)
+			{
+				// Create a Frame to act as the navigation context and navigate to the first page
+				rootFrame = new Frame { CacheSize = 2 };
 
-	            if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    
-                }
+				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+				{
 
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
+				}
 
-            if (rootFrame.Content == null)
-            {
-                // Removes the turnstile navigation for startup.
-                if (rootFrame.ContentTransitions != null)
-                {
-                    _transitions = new TransitionCollection();
-                    foreach (var c in rootFrame.ContentTransitions)
-                    {
-                        _transitions.Add(c);
-                    }
-                }
+				// Place the frame in the current Window
+				Window.Current.Content = rootFrame;
+			}
 
-                rootFrame.ContentTransitions = null;
-                rootFrame.Navigated += this.RootFrame_FirstNavigated;
+			if (rootFrame.Content == null)
+			{
+				// Removes the turnstile navigation for startup.
+				if (rootFrame.ContentTransitions != null)
+				{
+					_transitions = new TransitionCollection();
+					foreach (var c in rootFrame.ContentTransitions)
+					{
+						_transitions.Add(c);
+					}
+				}
 
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                if (!rootFrame.Navigate(typeof(StartPage), e.Arguments))
-                {
-                    throw new Exception("Failed to create initial page");
-                }
-            }
+				rootFrame.ContentTransitions = null;
+				rootFrame.Navigated += RootFrame_FirstNavigated;
+
+				// When the navigation stack isn't restored navigate to the first page,
+				// configuring the new page by passing required information as a navigation
+				// parameter
+				if (!rootFrame.Navigate(typeof(StartPage), e.Arguments))
+				{
+					throw new Exception("Failed to create initial page");
+				}
+			}
 
 			Window.Current.Activate();
 
-		    await MediaCaptureManager.InitializeCameraAsync();
-        }
+			await MediaCaptureManager.InitializeCameraAsync();
+		}
 
-        /// <summary>
-        /// Restores the content transitions after the app has launched.
-        /// </summary>
-        /// <param name="sender">The object where the handler is attached.</param>
-        /// <param name="e">Details about the navigation event.</param>
-        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
-        {
-            var rootFrame = sender as Frame;
-            rootFrame.ContentTransitions = this._transitions ?? new TransitionCollection() { new NavigationThemeTransition() };
-            rootFrame.Navigated -= this.RootFrame_FirstNavigated;
-        }
+		/// <summary>
+		/// Restores the content transitions after the app has launched.
+		/// </summary>
+		/// <param name="sender">The object where the handler is attached.</param>
+		/// <param name="e">Details about the navigation event.</param>
+		private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+		{
+			var rootFrame = sender as Frame;
+			if (rootFrame == null) return;
+			rootFrame.ContentTransitions = _transitions ?? new TransitionCollection { new NavigationThemeTransition() };
+			rootFrame.Navigated -= RootFrame_FirstNavigated;
+		}
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private async void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private static async void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
 
 			await MediaCaptureManager.CleanupCaptureResourcesAsync();
 
-            deferral.Complete();
-        }
+			deferral.Complete();
+		}
 
-	    private async void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+		private static async void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
 		{
 			await MediaCaptureManager.CleanupCaptureResourcesAsync();
 		}
-    }
+	}
 }
