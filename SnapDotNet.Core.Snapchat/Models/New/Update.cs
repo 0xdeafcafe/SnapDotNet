0ce5f6c8042697fe27cbd581d5a66d937babfcd5
Alex Reed (Xerax) <alexreed@outlook.com>
2014-06-04 18:41:59 +0100

    model work

---
+++ SnapDotNet.Core.Snapchat/Models/New/Update.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SnapDotNet.Core.Snapchat.Models.New
+{
+	public class Update
+	{
+		[DataMember(Name = "background_fetch_secret_key")]
+		public String BackgroundFetchSecretKey { get; set; }
+
+		[DataMember(Name = "conversations_response")]
+		public Conversation ConversationResponse { get; set; }
+	}
+}
