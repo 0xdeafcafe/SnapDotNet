3546d9ac1a9a0cf7fcef270b7d65b54e19bc29c8
CDillinger <collin.dillinger@gmail.com>
2014-06-05 19:34:35 -0400

    Start dynamic orientation changing

--- Snapchat.WindowsPhone/Pages/StartPage.xaml.cs
+++ Snapchat.WindowsPhone/Pages/StartPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xaml.Interactivity;
+using System;
 using System.Linq;
 using Windows.Phone.UI.Input;
 using Windows.UI.Xaml;
@@ -6,15 +7,24 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 using Snapchat.Helpers;
+using Windows.Graphics.Display;
+using Windows.Devices.Sensors;
+using Windows.UI.Core;
 
 namespace Snapchat.Pages
 {
     public sealed partial class StartPage : Page
-    {
+	{
+		private Accelerometer _accelerometer;
+
 		public StartPage()
-        {
+		{
+			NavigationCacheMode = NavigationCacheMode.Required;
+
             InitializeComponent();
-            NavigationCacheMode = NavigationCacheMode.Required;
+
+			// Detect orientation changes
+			_accelerometer = Accelerometer.GetDefault();
         }
 
 	    protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -23,6 +33,10 @@ namespace Snapchat.Pages
 
 		    var logoutTask = App.SnapchatManager.Logout();
 			var initializeCameraTask = MediaCaptureManager.InitializeCameraAsync();
+
+			// Detect orientation changed
+			if (_accelerometer != null)
+				_accelerometer.ReadingChanged += CalculateDeviceRotation;
 	    }
 
 	    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
@@ -39,6 +53,40 @@ namespace Snapchat.Pages
 			// Otherwise, go back to the start screen.
 			VisualStateManager.GoToState(VisualStateUtilities.FindNearestStatefulControl(PageContainer), "Start", false);
 			e.Handled = true;
+			
+			if (_accelerometer != null)
+				_accelerometer.ReadingChanged -= CalculateDeviceRotation;
+		}
+
+		private async void CalculateDeviceRotation(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+		{
+			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { UpdateOrientation(); });
+		}
+
+		private void UpdateOrientation()
+		{
+			switch (DisplayInformation.GetForCurrentView().CurrentOrientation)
+			{
+
+				case DisplayOrientations.Portrait:
+				case DisplayOrientations.PortraitFlipped:
+					if (StartPageGridLandscape.Visibility == Visibility.Visible)
+					{
+						StartPageGridPortrait.Visibility = Visibility.Visible;
+						StartPageGridLandscape.Visibility = Visibility.Collapsed;
+					}
+					break;
+
+				case DisplayOrientations.Landscape:
+				case DisplayOrientations.LandscapeFlipped:
+				default:
+					if (StartPageGridPortrait.Visibility == Visibility.Visible)
+					{
+						StartPageGridLandscape.Visibility = Visibility.Visible;
+						StartPageGridPortrait.Visibility = Visibility.Collapsed;
+					}
+					break;
+			}
 		}
 
 		private void LogInPageEmailUsernameTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
