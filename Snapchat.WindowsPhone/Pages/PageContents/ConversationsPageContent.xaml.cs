9227b2895dadb035188981df5521864e6a9ddd45
Matt <msaville8@gmail.com>
2014-06-07 22:32:05 -0400

    started work on the "swipe to open 1-on-1 chat" feature

--- Snapchat.WindowsPhone/Pages/PageContents/ConversationsPageContent.xaml.cs
+++ Snapchat.WindowsPhone/Pages/PageContents/ConversationsPageContent.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Snapchat.ViewModels.PageContents;
 using Windows.Phone.UI.Input;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace Snapchat.Pages.PageContents
 {
@@ -28,6 +30,28 @@ namespace Snapchat.Pages.PageContents
 		private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
 		{
 			MainPage.Singleton.RestoreBottomAppBar();
+		}
+
+		private void Grid_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+		{
+			var indicator = (sender as Grid).Children[0] as Grid;
+			double cumX = e.Cumulative.Translation.X;
+			indicator.Width = cumX > 0 ? cumX : 0;
+		}
+
+		private void Grid_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+		{
+			var container = sender as Grid;
+			var indicator = container.Children[0] as Grid;
+
+			if (indicator.ActualWidth >= container.ActualWidth) 
+			{
+				// TODO: Make main page ScrollViewer scroll to the newly available individual chat page.
+			}
+			else
+			{
+				// TODO: Go back to 0 with easing (invoke storyboard or something?)
+			}
 		}
 	}
 }
