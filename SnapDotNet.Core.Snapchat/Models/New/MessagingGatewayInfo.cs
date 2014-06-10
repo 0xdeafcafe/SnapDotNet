71afb7baeb72f69dfe6cb2827d86d064cb60e2b1
Alex Reed (Xerax) <alexreed@outlook.com>
2014-06-10 14:52:11 +0100

    Made MessagingGatewayInfo fully Inpc

--- SnapDotNet.Core.Snapchat/Models/New/MessagingGatewayInfo.cs
+++ SnapDotNet.Core.Snapchat/Models/New/MessagingGatewayInfo.cs
@@ -9,10 +9,20 @@ namespace SnapDotNet.Core.Snapchat.Models.New
 		: NotifyPropertyChangedBase
 	{
 		[DataMember(Name = "gateway_auth_token")]
-		public GatewayAuthenticationToken GatewayAuthenticationToken { get; set; }
+		public GatewayAuthenticationToken GatewayAuthenticationToken
+		{
+			get { return _gatewayAuthenticationToken; }
+			set { SetField(ref _gatewayAuthenticationToken, value); }
+		}
+		private GatewayAuthenticationToken _gatewayAuthenticationToken;
 
 		[DataMember(Name = "gateway_server")]
-		public String GatewayServer { get; set; }
+		public String GatewayServer
+		{
+			get { return _gatewayServer; }
+			set { SetField(ref _gatewayServer, value); }
+		}
+		private String _gatewayServer;
 	}
 
 	[DataContract]
@@ -20,9 +30,19 @@ namespace SnapDotNet.Core.Snapchat.Models.New
 		: NotifyPropertyChangedBase
 	{
 		[DataMember(Name = "mac")]
-		public String Mac { get; set; }
+		public String Mac
+		{
+			get { return _mac; }
+			set { SetField(ref _mac, value); }
+		}
+		private String _mac;
 
 		[DataMember(Name = "payload")]
-		public String Payload { get; set; }
+		public String Payload
+		{
+			get { return _payload; }
+			set { SetField(ref _payload, value); }
+		}
+		private String _payload;
 	}
 }
